fa4cce473a93e568422ad30829f19fe0077b9b14
Barby971 <barbara.lefebvre23@gmail.com>
2021-05-10 21:45:00 +0200

    ajoute ft_putchar.c

---
+++ C00/Ex00/ft_putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+void ft_putchar(char c ){
+	write(1, &c, 1);
+}
+
+
+int main(void){
+	ft_putchar('a');
+	ft_putchar('l');
+	ft_putchar('l');
+	ft_putchar('l');
+	ft_putchar('l');
+	ft_putchar('l');
+	ft_putchar('l');
+	ft_putchar('l');
+	ft_putchar('\n');
+	return(0);
+}
+
