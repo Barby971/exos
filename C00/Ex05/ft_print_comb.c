780b555136720895b9632ba70c20c6d4a1bab8f8
Barby971 <barbara.lefebvre23@gmail.com>
2021-05-30 12:00:37 +0200

    ajouter ft_print_comb v3

--- C00/Ex05/ft_print_comb.c
+++ C00/Ex05/ft_print_comb.c
@@ -12,30 +12,28 @@ void ft_print_comb(void)
 	char second = 1;
 	char third = 2;
 
-	first ='0';
 	while (first <= '7');
 	{
-		second = first +1;
 			while (second <= '8');
 			{
-				third > '2';
+	
 				while (third <='9');
+	
 					{
 				
 					ft_putchar(first);
 					ft_putchar(second);
 					ft_putchar(third);
-					{
 						ft_putchar(',');
 						ft_putchar(' ');
 					}
-				third ++;
-				}
-			second++;	
+			
+			third++;
+		
 			}
-		first++;	
+		second++;	
 	}
-
+first++;
 }
 
 int main(void)
